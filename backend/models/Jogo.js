2cf2c553825c73fdd04de4edbb6d81367950ff92
Alexandre <alexandre.peder@escola.pr.gov.br>
2024-08-18 16:36:32 -0300

    correção de bug: erro de digitação do require da constante mysql

--- backend/models/Jogo.js
+++ backend/models/Jogo.js
@@ -1,9 +1,9 @@
-const mysql = requise('mysql12');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user = 'root',
-    password: '1963';
+    user: 'root',
+    password: '1963',
     database: 'encontra_jogo_volei'
 });
 
